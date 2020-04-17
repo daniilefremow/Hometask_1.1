d2f2905f4c5ba1e15f3fd93339e863d8f16a2df3
DaniilEfremow <klevermilka@gmail.com>
2020-04-17 21:08:45 +0300

    Add hometask_1.1

---
+++ Hometask_1.1/Hometask_1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hometask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число cтрок массива: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число столбцов массива: ");
+            int m = int.Parse(Console.ReadLine());
+            int[,] mas = new int[n, m];
+            int summa = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.WriteLine("Введите mas[{0},{1}]", i, j);
+                    mas[i, j] = int.Parse(Console.ReadLine());
+
+                    if (mas[i, j] % 2 == 0)
+                    {
+                        summa += mas[i, j];
+                    }
+                }
+
+            }
+            Console.WriteLine("Сумма всех четных элементов = {0}", summa);
+            Console.ReadLine();
+        }
+    }
+}
